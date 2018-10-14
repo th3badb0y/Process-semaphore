ff4ff884c7708c52ea535a5d0118760a25b91c6b
Yaswanth ps <yaswanthyadu@gmail.com>
2018-10-14 14:06:55 +0530

    update process.c

--- process.c
+++ process.c
@@ -3,29 +3,29 @@
 #include<sys/types.h>
 main()
 {
- 	int i,p;
+ 	int i,n;
  	printf("\n pid=%d, ppid=%d",getpid(),getppid());
  	p=fork();
- 	if(p == 0)
+ 	if(n == 0)
  	{
  		for(i=0;i<3;i++)
  			{
- 				printf("\n Child process running,pid=%d,ppid=%d",getpid(),getppid());
+ 				printf("\n Child process is running,pid=%d,ppid=%d",getpid(),getppid());
  				sleep(2);
  			}
  			printf("\n Child Exited\n");
  			sleep(2);
  	}
- 	else if(p>0)
+ 	else if(n>0)
  	{
  		for(i=0;i<6;i++)
  			{
- 		 		printf("\n Parent process running pid=%d,ppid=%d",getpid(),getppid());
+ 		 		printf("\n Parent process is running pid=%d,ppid=%d",getpid(),getppid());
  		 		sleep(2);
  		 	}
  	}
  	else
- 		printf("\n Error in creating chid process");
+ 		printf("\n Error in creating child process");
  }
  	
  		  
